c44d06d70ac6a3ce814679bd3427bd85de35b76f
nickveil <31191433+nickveil@users.noreply.github.com>
2020-08-18 11:24:21 -0500

    fixed the integer col and int data issue

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,15 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Product;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
 
     return [
         'name'=> $faker->sentence(6, true),
         'count'=> $faker->randomDigit,
-        'price'=> $faker->randomFloat(2, 0.00, 10000.00),
+        'price'=> $faker->randomNumber(),
         'description'=> $faker->realText(150, 1),
         'image'=> $faker->imageUrl(800, 600,'abstract')
         
