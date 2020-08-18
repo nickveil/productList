025f803f7e00aa4a207315bfd9c2312bcbe3cd27
nickveil <31191433+nickveil@users.noreply.github.com>
2020-08-18 10:19:44 -0500

    dummy seed data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,13 @@ use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
+
     return [
-        //
+        'name'=> $faker->sentence(6, true),
+        'count'=> $faker->randomDigit,
+        'price'=> $faker->randomFloat(2, 0.00, 10000.00),
+        'description'=> $faker->realText(150, 1),
+        'image'=> $faker->imageUrl(800, 600,'abstract')
+        
     ];
 });
